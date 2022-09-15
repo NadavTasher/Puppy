b942934df1e6e059ffae86debdb05cad0794685f
Nadav Tasher <tashernadav@gmail.com>
2022-09-15 19:25:15 +0000

    Updated looper to be context

--- puppy/thread/looper.py
+++ puppy/thread/looper.py
@@ -5,12 +5,21 @@ from threading import Thread, Event, Lock
 class Looper(Thread):
     def __init__(self, parent=None):
         # Initialize internal variables
-        self._lock = Lock()
-        self._event = Event()
-        self._parent = parent
+        self.lock = Lock()
+        self.event = Event()
+        self.parent = parent
 
         # Initialize thread class
         super(Looper, self).__init__()
+
+    def __enter__(self):
+        # Initialize self
+        self.initialize()
+
+    def __exit__(self, *_):
+        # Finalize self
+        self.finalize()
+        return False
 
     def loop(self):
         raise NotImplementedError()
@@ -24,12 +33,12 @@ class Looper(Thread):
     @property
     def running(self):
         # Check if event is set
-        if self._event.is_set():
+        if self.event.is_set():
             return False
 
         # Check if parent is running
-        if self._parent:
-            if not self._parent.running:
+        if self.parent:
+            if not self.parent.running:
                 return False
 
         # Still running!
@@ -37,7 +46,7 @@ class Looper(Thread):
 
     def reset(self):
         # Acquire the lock and restart
-        with self._lock:
+        with self.lock:
             # Finalize the looper
             self.finalize()
 
@@ -46,26 +55,21 @@ class Looper(Thread):
 
     def stop(self, wait=False):
         # Set the event
-        self._event.set()
+        self.event.set()
 
         # Wait for the thread to finish
         if wait:
             self.join()
 
     def run(self):
-        try:
-            # Initialize looper
-            self.initialize()
-
+        # Use context manager
+        with self:
             # Loop until shutdown
             try:
                 while self.running:
                     # Acquire lock before looping
-                    with self._lock:
+                    with self.lock:
                         self.loop()
             finally:
                 # Stop self to kill children
                 self.stop()
-        finally:
-            # Finalize looper
-            self.finalize()
