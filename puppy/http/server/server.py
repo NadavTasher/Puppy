2ff958fb3f8a165b9972fc73936e8c388d51b8e6
Nadav Tasher <tashernadav@gmail.com>
2022-11-26 20:56:57 +0000

    WIP - not working server

--- puppy/http/server/server.py
+++ puppy/http/server/server.py
@@ -4,68 +4,122 @@ import select  # NOQA
 
 from puppy.http.http import HTTP  # NOQA
 from puppy.http.mixins import HTTPSafeReceiverMixIn  # NOQA
-from puppy.socket.server import ThreadingTCPServer, StreamRequestHandler  # NOQA
+# from puppy.socket.server import SocketServer, SocketWorker
+
+from puppy.thread.looper import Looper
 
 
 class HTTPClass(HTTP, HTTPSafeReceiverMixIn):
     pass
 
 
-class HTTPWorker(StreamRequestHandler, object):
-    def handle(self):
-        # Create an interface from the socket
-        interface = HTTPClass(self.request)
+class MultiHTTPWorker(Looper):
+    def __init__(self, server, handler, context=None):
+        # Initialize the looper
+        super(MultiHTTPWorker, self).__init__()
+        
+        # Set daemon to true
+        self.daemon = True
 
-        # Loop until interface is closed
-        try:
-            while not interface.closed:
-                # Check if interface is readable
-                if not interface.wait(1):
-                    continue
+        # Set the internal state
+        self._socket = None
+        self._interface = None
 
-                # Read request from client
-                request = interface.receive_request()
+        # Set internal constants
+        self._server = server
+        self._handler = handler
+        self._context = context
 
-                # Handle client request
-                response = self.server.handler(request)
+    def loop(self):
+        # Check if interface was closed
+        if self._interface.closed:
+            self.stop()
+            return
 
-                # Transmit response to client
-                interface.transmit_response(response)
-        except:
-            # Abort the connection
-            interface.abort()
-        finally:
-            # Close the interface
-            interface.close()
+        # Check if interface is readable
+        if not self._interface.wait(1):
+            return
+
+        # Read request from client
+        request = self._interface.receive_request()
+
+        # Handle client request
+        response = self._handler(request)
+
+        # Transmit response to client
+        self._interface.transmit_response(response)
+
+    def initialize(self):
+        # Accept a client
+        self._socket, address = self._server.accept()
+
+        # Set the internal name
+        self.name = "%s:%d" % address
+
+        # If a context was defined, wrap socket
+        if self._context:
+            self._socket = self._context.wrap_socket(self._socket, server_side=True)
+
+        # Initialize the interface
+        self._interface = HTTPClass(self._socket)
+
+    def finalize(self):
+        # Close socket if needed
+        if self._socket:
+            self._socket.close()
 
 
-class HTTPSWorker(HTTPWorker):
-    def handle(self):
-        # Wrap with TLS implementation
-        self.request = self.server.context.wrap_socket(self.request, server_side=True)
+class MultiHTTPServer(Looper):
+    def __init__(self, handler, http_port=80, https_port=443):
+        # Initialize looper
+        super(MultiHTTPServer, self).__init__()
 
-        # Handle using parent
-        return super(HTTPSWorker, self).handle()
+        # Set daemon to true
+        self.daemon = True
 
+        # Set internal variables
+        self._handler = handler
+        self._http_port = http_port
+        self._https_port = https_port
 
-class HTTPServer(ThreadingTCPServer, object):
-    def __init__(self, address, handler):
-        # Set the handler
-        self.handler = handler
+        # Initialize sockets for listening
+        self._http_socket = None
+        self._https_socket = None
 
-        # Initialize parent
-        super(HTTPServer, self).__init__(address, HTTPWorker)
+        # Initialize ssl context
+        self._context = ssl.create_default_context()
+        self._context.check_hostname = False
+        self._context.verify_mode = ssl.CERT_OPTIONAL
 
+    @property
+    def context(self):
+        return self._context
 
-class HTTPSServer(ThreadingTCPServer, object):
-    def __init__(self, address, handler):
-        # Set the handler
-        self.handler = handler
+    def loop(self):
+        # Check which sockets are readable
+        readable_sockets, _, _ = select.select([self._http_socket, self._https_socket], [], [], 0.5)
 
-        # Create a new context
-        self.context = ssl.create_default_context()
-        self.context.check_hostname = False
-        self.context.verify_mode = ssl.CERT_OPTIONAL
+        # Loop over readable sockets and create client threads
+        for readable_socket in readable_sockets:
+            worker = MultiHTTPWorker(readable_socket, self._handler, self._context if readable_socket is self._https_socket else None)
+            worker.start()
 
-        # Initialize parent
-        super(HTTPSServer, self).__init__(address, HTTPSWorker)
+    def initialize(self):
+        # Create HTTP socket for listening
+        self._http_socket = socket.socket()
+        self._http_socket.bind(("0.0.0.0", self._http_port))
+        self._http_socket.listen(1)
+
+        # Create HTTPS socket for listening
+        self._https_socket = socket.socket()
+        self._https_socket.bind(("0.0.0.0", self._https_port))
+        self._https_socket.listen(1)
+
+    def finalize(self):
+        # Close HTTP socket if needed
+        if self._http_socket:
+            self._http_socket.close()
+
+        # Close HTTPS socket if needed
+        if self._https_socket:
+            self._https_socket.close()
--- wip.py
+++ wip.py
@@ -3,20 +3,20 @@ import logging
 
 
 from puppy.http.client.client import HTTPClient
-from puppy.http.server.server import HTTPServer
+from puppy.http.server.server import MultiHTTPServer
 from puppy.http.server.router import HTTPRouter
 from puppy.http.types import Response
 
-# rtr = HTTPRouter()
-# rtr.static(".")
+rtr = HTTPRouter()
+rtr.static(".")
 
-# @rtr.get("/test")
-# def handle(request):
-# 	return Response(200, "OK", None, "Hello World!")
+@rtr.get("/test")
+def handle(request):
+	return Response(200, "OK", None, "Hello World!")
 
-# @rtr.get("/crash")
-# def h2(request):
-# 	assert not request
+@rtr.get("/crash")
+def h2(request):
+	assert not request
 
 # cli = HTTPClient()
 # srv = HTTPServer(("0.0.0.0", 8000), rtr)
@@ -35,30 +35,34 @@ from puppy.http.types import Response
 # srv.join()
 # print(cli.get(b"https://example.com/hello"))
 
-from puppy.thread.machine import Machine, state
+# from puppy.thread.machine import Machine, state
 
-class TestMachine(Machine):
-	_counter = 0
+# class TestMachine(Machine):
+# 	_counter = 0
 
-	@state
-	def start_state(self):
-		return self.check_state
+# 	@state
+# 	def start_state(self):
+# 		return self.check_state
 
-	@state
-	def check_state(self):
-		if self._counter > 30:
-			return self.stop_state
+# 	@state
+# 	def check_state(self):
+# 		if self._counter > 30:
+# 			return self.stop_state
 		
-		return self.increment_state
+# 		return self.increment_state
 
-	@state
-	def increment_state(self):
-		self._counter += 1
-		time.sleep(0.5)
-		return self.check_state, "Incremented to %d" % self._counter
+# 	@state
+# 	def increment_state(self):
+# 		self._counter += 1
+# 		time.sleep(0.5)
+# 		return self.check_state, "Incremented to %d" % self._counter
 
-logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
 
-m = TestMachine()
-m.start()
-m.join()
+# m = TestMachine()
+# m.start()
+# m.join()
+
+s = MultiHTTPServer(rtr)
+s.start()
+s.join()
