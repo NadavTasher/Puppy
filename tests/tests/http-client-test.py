c797215860145014040f3b79e85a86f246c57b3c
Nadav Tasher <tashernadav@gmail.com>
2022-12-03 15:19:39 +0000

    Cleanups

--- tests/tests/http-client-test.py
+++
@@ -1,5 +0,0 @@
-from puppy.http.client.client import HTTPClient
-
-client = HTTPClient()
-
-print(client.get("http://example.com/"))
--- tests/tests/logging-test.py
+++
@@ -1,9 +0,0 @@
-import logging
-from puppy.utilities.log import setup
-
-setup()
-
-print("AAAA")
-logging.info("Hello")
-logging.warning("Hello World")
-logging.error("Hello World!")
