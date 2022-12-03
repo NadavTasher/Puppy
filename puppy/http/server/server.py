1023366a145d8df47af7c43b257cbd5701a38ff2
Nadav Tasher <tashernadav@gmail.com>
2022-12-03 15:09:04 +0000

    WIP test

--- puppy/http/server/server.py
+++ puppy/http/server/server.py
@@ -4,31 +4,20 @@ import select  # NOQA
 
 from puppy.http.http import HTTP  # NOQA
 from puppy.http.mixins import HTTPSafeReceiverMixIn  # NOQA
+
 # from puppy.socket.server import SocketServer, SocketWorker
 
 from puppy.thread.looper import Looper
+from puppy.socket.server import SocketServer, SocketWorker  # NOQA
 
 
 class HTTPClass(HTTP, HTTPSafeReceiverMixIn):
     pass
 
 
-class HTTPWorker(Looper):
-    def __init__(self, server, handler, context=None):
-        # Initialize the looper
-        super(HTTPWorker, self).__init__()
-        
-        # Set daemon to true
-        self.daemon = True
-
-        # Set the internal state
-        self._socket = None
-        self._interface = None
-
-        # Set internal constants
-        self._server = server
-        self._handler = handler
-        self._context = context
+class HTTPHandler(SocketWorker):
+    # Internal HTTP interface
+    _interface = None
 
     def loop(self):
         # Check if interface was closed
@@ -44,88 +33,100 @@ class HTTPWorker(Looper):
         request = self._interface.receive_request()
 
         # Handle client request
-        response = self._handler(request)
+        response = self._parent._handler(request)
 
         # Transmit response to client
         self._interface.transmit_response(response)
 
+
+class HTTPWorker(HTTPHandler):
     def initialize(self):
-        # Accept a client
-        self._socket, address = self._server.accept()
-
-        # Set the internal name
-        self.name = "%s:%d" % address
-
-        # If a context was defined, wrap socket
-        if self._context:
-            self._socket = self._context.wrap_socket(self._socket, server_side=True)
+        # Initialize parent
+        super(HTTPWorker, self).initialize()
 
         # Initialize the interface
         self._interface = HTTPClass(self._socket)
 
-    def finalize(self):
-        # Close socket if needed
-        if self._socket:
-            self._socket.close()
+
+class HTTPSWorker(HTTPHandler):
+    def initialize(self):
+        # Initialize parent
+        super(HTTPSWorker, self).initialize()
+
+        # If a context was defined, wrap socket
+        if self._context:
+            self._socket = self._parent.context.wrap_socket(
+                self._socket, server_side=True
+            )
+
+        # Initialize the interface
+        self._interface = HTTPClass(self._socket)
 
 
-class HTTPServer(Looper):
-    def __init__(self, handler, http_port=80, https_port=443):
+# class HTTPWorker(Looper):
+#     def __init__(self, server, handler, context=None):
+#         # Initialize the looper
+#         super(HTTPWorker, self).__init__()
+
+#         # Set daemon to true
+#         self.daemon = True
+
+#         # Set the internal state
+#         self._socket = None
+#         self._interface = None
+
+#         # Set internal constants
+#         self._server = server
+#         self._handler = handler
+#         self._context = context
+
+#     def loop(self):
+#         # Check if interface was closed
+#         if self._interface.closed:
+#             self.stop()
+#             return
+
+#         # Check if interface is readable
+#         if not self._interface.wait(1):
+#             return
+
+#         # Read request from client
+#         request = self._interface.receive_request()
+
+#         # Handle client request
+#         response = self._handler(request)
+
+#         # Transmit response to client
+#         self._interface.transmit_response(response)
+
+#     def initialize(self):
+#         # Accept a client
+#         self._socket, address = self._server.accept()
+
+#         # Set the internal name
+#         self.name = "%s:%d" % address
+
+#         # If a context was defined, wrap socket
+#         if self._context:
+#             self._socket = self._context.wrap_socket(self._socket, server_side=True)
+
+#         # Initialize the interface
+#         self._interface = HTTPClass(self._socket)
+
+#     def finalize(self):
+#         # Close socket if needed
+#         if self._socket:
+#             self._socket.close()
+
+
+class HTTPServer(SocketServer):
+    def __init__(self, handler, http=80, https=443):
         # Initialize looper
-        super(HTTPServer, self).__init__()
-
-        # Set daemon to true
-        self.daemon = True
-
-        # Set internal variables
-        self._handler = handler
-        self._http_port = http_port
-        self._https_port = https_port
-
-        # Initialize sockets for listening
-        self._http_socket = None
-        self._https_socket = None
+        super(HTTPServer, self).__init__(
+            {("0.0.0.0", http): HTTPWorker, ("0.0.0.0", https): HTTPSWorker}
+        )
 
         # Initialize ssl context
-        self._context = ssl.create_default_context()
-        self._context.check_hostname = False
-        self._context.verify_mode = ssl.CERT_OPTIONAL
-
-    @property
-    def context(self):
-        return self._context
-
-    def loop(self):
-        # Check which sockets are readable
-        readable_sockets, _, _ = select.select([self._http_socket, self._https_socket], [], [], 0.5)
-
-        # Loop over readable sockets and create client threads
-        for readable_socket in readable_sockets:
-            # Create new worker
-            if readable_socket is self._https_socket:
-                worker = HTTPWorker(readable_socket, self._handler, self._context)
-            else:
-                worker = HTTPWorker(readable_socket, self._handler)
-            
-            # Start the new worker
-            worker.start()
-
-    def initialize(self):
-        # Create HTTP socket for listening
-        self._http_socket = socket.socket()
-        self._http_socket.bind(("0.0.0.0", self._http_port))
-        self._http_socket.listen(1)
-
-        # Create HTTPS socket for listening
-        self._https_socket = socket.socket()
-        self._https_socket.bind(("0.0.0.0", self._https_port))
-        self._https_socket.listen(1)
-
-    def finalize(self):
-        # Close HTTP socket if needed
-        if self._http_socket:
-            self._http_socket.close()
-
-        # Close HTTPS socket if needed
-        if self._https_socket:
-            self._https_socket.close()
+        self.context = ssl.create_default_context()
+        self.context.check_hostname = False
+        self.context.verify_mode = ssl.CERT_OPTIONAL
--- puppy/socket/server.py
+++ puppy/socket/server.py
@@ -1,10 +1,3 @@
-try:
-    # Python 3 socketserver
-    from socketserver import *
-except:
-    # Python 2 socketserver
-    from SocketServer import *
-
 import socket  # NOQA
 import select  # NOQA
 
@@ -16,52 +9,75 @@ SOCKET_TIMEOUT = 10
 
 
 class SocketServer(Looper):
-    def __init__(self, address, implementation):
+    def __init__(self, addresses):
         # Initialize parent
         super(SocketServer, self).__init__()
 
         # Set internal state
-        self._socket = None
-        self._address = address
-        self._implementation = implementation
+        self._servers = list()
+        self._addresses = addresses
+
+    def create(self, address):
+        # Create new server socket
+        server = socket.socket()
+        server.bind(address)
+        server.listen(1)
+
+        return server
 
     def loop(self):
         # Check if socket is readable
-        ready, _, _ = select.select([self._socket], [], [], SELECT_TIMEOUT)
+        readable_servers, _, _ = select.select([self._servers], [], [], SELECT_TIMEOUT)
 
         # Make sure socket is ready
-        if not ready:
+        if not readable_servers:
             return
 
-        # Run new worker thread
-        self._implementation(self._socket).start()
+        # Create a new worker for each socket
+        for readable_socket in readable_servers:
+            # Get address of socket
+            address = readable_socket.getsockname()
+
+            # Create new worker
+            worker = self._addresses[address](self, readable_socket)
+            worker.start()
 
     def initialize(self):
-        self._socket = socket.socket()
-        self._socket.bind(self._address)
-        self._socket.listen(1)
+        # Loop over addresses
+        for address in self._addresses.keys():
+            # Create new server
+            server = self.create(address)
+
+            # Add server to list
+            self._servers.append(server)
 
     def finalize(self):
-        if self._socket:
-            self._socket.close()
+        # Close all servers
+        for server in self._servers:
+            server.close()
 
 
 class SocketWorker(Looper):
-    def __init__(self, server):
+    def __init__(self, parent, server):
         # Initialize parent
         super(SocketWorker, self).__init__()
 
+        # Set daemon property
+        self.daemon = True
+
         # Store connection internally
         self._socket = None
+        self._parent = parent
         self._server = server
 
     def initialize(self):
         # Accept new connection
-        self._socket, address = self._server.accept()
+        self._socket, address = self._server._socket.accept()
 
         # Set new name
         self.name = "%s:%d" % address
 
     def finalize(self):
+        # Close socket connection
         if self._socket:
             self._socket.close()
