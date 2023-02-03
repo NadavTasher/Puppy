607f459859b436ee15512d7ec10b9b095b76f59f
Nadav Tasher <tashernadav@gmail.com>
2023-02-03 13:34:40 +0000

    WIP

--- puppy/database.py
+++ puppy/database.py
@@ -5,7 +5,7 @@ import threading
 import contextlib
 import collections
 
-from puppy.bunch import MutableBunch
+from puppy.bunch import MutableBunch, Bunch
 from puppy.filesystem import remove
 
 class Index(object):
@@ -23,11 +23,11 @@ class Index(object):
 		with self.lock:
 			# Make sure index exists
 			if not os.path.exists(self.path):
-				return list()
+				return set()
 
 			# Read index contents
 			with open(self.path, "r") as file:
-				return json.load(file)
+				return set(json.load(file))
 
 	@contextlib.contextmanager
 	def modify(self):
@@ -40,13 +40,13 @@ class Index(object):
 
 			# Write to file
 			with open(self.path, "w") as file:
-				json.dump(index, file)
+				json.dump(list(index), file)
 
 class Objects(object):
-	def __init__(self, path):
+	def __init__(self, path, locks):
 		# Create variables
 		self.path = os.path.join(path, self.__class__.__name__.lower())
-		self.locks = dict()
+		self.locks = locks
 
 		# Create objects path if it does not exist
 		if not os.path.exists(self.path):
@@ -57,14 +57,17 @@ class Objects(object):
 	
 	@contextlib.contextmanager
 	def modify(self, name):
+		# Create the path
+		path = self.read(name)
+
 		# Check mutex for name if it does not exist
-		if name not in self.locks:
-			self.locks[name] = threading.RLock()
+		if path not in self.locks:
+			self.locks[path] = threading.RLock()
 
 		# Lock the mutex
-		with self.locks[name]:
-			# Yield the combined path
-			yield self.read(name)
+		with self.locks[path]:
+			# Yield the object path
+			yield path
 
 
 class Keystore(MutableBunch):
@@ -72,16 +75,17 @@ class Keystore(MutableBunch):
 	index = None
 	objects = None
 
-	def __init__(self, path):
-		super(Keystore, self).__init__()
+	# Define default variable
+	DEFAULT = object()
 
+	def __init__(self, path, locks):
 		# Create directory if it does not exist
 		if not os.path.exists(path):
 			os.makedirs(path)
 
 		# Create managing objects
 		self.index = Index(path)
-		self.objects = Objects(path)
+		self.objects = Objects(path, locks)
 
 	def __contains__(self, key):
 		# Make sure file exists
@@ -106,33 +110,33 @@ class Keystore(MutableBunch):
 				return json.load(file)
 
 		# Create a complex object from the path
-		return self.__class__(path)
+		return Keystore(path, self.objects.locks)
 
 
 	def __setitem__(self, key, value):
 		# Modify the object
 		with self.objects.modify(key) as path:	
 			# Check if value is a dictionary
-			if not isinstance(value, dict):
+			if not isinstance(value, collections.Mapping):
 				# Make sure value is JSON seriallizable
 				json.dumps(value)
+
+				# Delete the old value
+				remove(path)
 
 				# Write the object data as string
 				with open(path, "w") as file:
 					json.dump(value, file)
 			else:
 				# Delete the old value
-				if key in self:
-					del self[key]
+				remove(path)
 
 				# Create a new keystore
-				self.__class__(path).update(value)
+				Keystore(path, self.objects.locks).update(value)
 
 		# Check if key needs to be added to index
-		if key not in self:
-			with self.index.modify() as index:
-				if key not in index:
-					index.append(key)
+		with self.index.modify() as index:
+			index.add(key)
 
 	def __delitem__(self, key):
 		# Make sure key exists
@@ -141,11 +145,11 @@ class Keystore(MutableBunch):
 
 		# Delete item from index
 		with self.index.modify() as index:
-			if key in index:
-				index.remove(key)
+			index.discard(key)
 
 		# Delete item from filesystem
-		remove(self.objects.read(key))
+		with self.objects.modify(key) as path:	
+			remove(path)
 
 	def __iter__(self):
 		# Read the index
@@ -155,7 +159,7 @@ class Keystore(MutableBunch):
 
 	def __len__(self):
 		# Calculate the length of keys
-		return len(self)
+		return len(iter(self))
 		
 	def keys(self):
 		# Loop over keys
@@ -184,15 +188,26 @@ class Keystore(MutableBunch):
 		# Return the value
 		return self[key]
 
-	def pop(self, key, default=None):
-		# TODO: fix default variable
+	def pop(self, key, default=DEFAULT):
 		try:
+			# Fetch the value
 			value = self[key]
+
+			# Check if the value is a keystore
+			if isinstance(value, Keystore):
+				value = value.copy()
+			
+			# Delete the item
 			del self[key]
+
+			# Return the value
 			return value
 		except KeyError:
-			if default is not None:
+			# Check if a default is defined
+			if default != Keystore.DEFAULT:
 				return default
+
+			# Reraise exception
 			raise
 
 	def popitem(self):
@@ -212,7 +227,7 @@ class Keystore(MutableBunch):
 			value = self[key]
 
 			# Check if value is a keystore
-			if isinstance(value, self.__class__):
+			if isinstance(value, Keystore):
 				value = value.copy()
 
 			# Update the bunch
@@ -238,6 +253,19 @@ class Keystore(MutableBunch):
 		for key in kwargs:
 			self[key] = kwargs[key]
 
+	def setdefault(self, key, default=None):
+		# Check if key exists
+		if key not in self:
+			self[key] = value
+
+		# Return the value
+		return self[key]
+
 	def __repr__(self):
 		# Format the data like a dictionary
 		return "{%s}" % ", ".join("%r: %r" % item for item in self.items())
+
+class Database(Keystore):
+	def __init__(self, path):
+		# Initialize the keystore with an empty locker
+		super(Database, self).__init__(path, dict())
