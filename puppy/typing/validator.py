a26bfb1a19101533c248b591f0e36ef71eba487e
Nadav Tasher <tashernadav@gmail.com>
2023-02-09 20:52:44 +0000

    WIP

--- puppy/typing/validator.py
+++ puppy/typing/validator.py
@@ -14,26 +14,44 @@ class partial(object):
     def __repr__(self):
         return "%r%s" % (self._target, str(list(self._arguments)))
 
+# def validator(function):
+#     class Validator(type):
+#         @classmethod
+#         def __instancecheck__(cls, value):
+#             return function(value)
 
-class validator(object):
-    # Wrapper constructor
-    def __init__(self, target):
-        # Set internal function
-        self._target = target
+#         @staticmethod
+#         def __getitem__(argument):
+#             pass
+#     return Validator
 
-    def __call__(self, value, *arguments):
-        # Run type validation
-        return self._target(value, *arguments)
+def validator(function):
+    # Create validator object
+    class Type(object):
+        def __init__(self, *arguments):
+            self.arguments = arguments
 
-    def __getitem__(self, argument):
-        # Convert index into list
-        if isinstance(argument, tuple):
-            arguments = list(argument)
-        else:
-            arguments = [argument]
+        def __instancecheck__(self, value):
+            print("aaaaa")
+            return function(value, *self.arguments)
 
-        # Return a partial validator
-        return partial(self, arguments)
+        def __getitem__(self, argument):
+            # Convert index into list
+            if isinstance(argument, tuple):
+                arguments = list(argument)
+            else:
+                arguments = [argument]
 
-    def __repr__(self):
-        return self._target.__name__
+            # Return a partial validator
+            return self.__class__(*arguments)
+
+        def __repr__(self):
+            # Check if arguments are present
+            if not self.arguments:
+                return function.__name__
+
+            # Append arguments to name
+            return "%r%s" % (function.__name__, str(list(self.arguments)))
+    
+    # Return an instance of the type
+    return Type()
