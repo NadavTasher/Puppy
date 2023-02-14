20f9f6e09c8cf50508635935eda24008b146910c
Nadav Tasher <tashernadav@gmail.com>
2023-02-14 19:59:32 +0000

    Some tests

--- puppy/test/typing.py
+++ puppy/test/typing.py
@@ -11,3 +11,12 @@ class TypingTestCase(unittest.TestCase):
 		assert isinstance(None, Optional[Text])
 		assert isinstance("Hello World", Optional[Text])
 		assert not isinstance(0, Optional[Text])
+
+	def test_text(self):
+		assert isinstance("Hello World", Text)
+		assert isinstance(u"Hello World", Text)
+		assert not isinstance(0, Text)
+
+	def test_bytes(self):
+		assert isinstance(b"Hello World", Bytes)
+		assert not isinstance(0, Bytes)
