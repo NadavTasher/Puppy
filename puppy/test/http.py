af2ee1e90d6a23f2219dbfa7ce274162d938e9d3
Nadav Tasher <tashernadav@gmail.com>
2023-02-17 09:16:42 +0000

    WIP on tests

--- puppy/test/http.py
+++ puppy/test/http.py
@@ -4,25 +4,26 @@ from puppy.http.client import HTTPClient
 from puppy.http.server import HTTPServer
 from puppy.http.router import HTTPRouter
 
+
 class HTTPClientTestCase(unittest.TestCase):
-	def test_http(self):
-		# Create HTTP router for tests
-		router = HTTPRouter()
-		
-		# Register a route for tests
-		@router.get("/hello")
-		def _route(request):
-			return Response(200, "OK", None, "Test passed")
 
-		# Create HTTP server for tests
-		server = HTTPServer(router)
-		server.
+    def test_http(self):
+        # Create HTTP router for tests
+        router = HTTPRouter()
 
-		# Create HTTP client for tests
-		client = HTTPClient()
-		response = client.get("http://localhost/hello")
+        # Register a route for tests
+        @router.get("/hello")
+        def _route(request):
+            return Response(200, "OK", None, "Test passed")
 
-		assert response.status == 200
+        # Create HTTP server for tests
+        server = HTTPServer(router)
 
-	def run_server_in_new_thread(self):
-		pass
+        # Create HTTP client for tests
+        client = HTTPClient()
+        response = client.get("http://localhost/hello")
+
+        assert response.status == 200
+
+    def run_server_in_new_thread(self):
+        pass
--- puppy/test/typing.py
+++ puppy/test/typing.py
@@ -2,21 +2,23 @@ import unittest
 
 from puppy.typing.types import *
 
+
 class TypingTestCase(unittest.TestCase):
-	def test_any(self):
-		assert isinstance(None, Any)
-		assert isinstance("Hello World", Any)
 
-	def test_optional(self):
-		assert isinstance(None, Optional[Text])
-		assert isinstance("Hello World", Optional[Text])
-		assert not isinstance(0, Optional[Text])
+    def test_any(self):
+        assert isinstance(None, Any)
+        assert isinstance("Hello World", Any)
 
-	def test_text(self):
-		assert isinstance("Hello World", Text)
-		assert isinstance(u"Hello World", Text)
-		assert not isinstance(0, Text)
+    def test_optional(self):
+        assert isinstance(None, Optional[Text])
+        assert isinstance("Hello World", Optional[Text])
+        assert not isinstance(0, Optional[Text])
 
-	def test_bytes(self):
-		assert isinstance(b"Hello World", Bytes)
-		assert not isinstance(0, Bytes)
+    def test_text(self):
+        assert isinstance("Hello World", Text)
+        assert isinstance(u"Hello World", Text)
+        assert not isinstance(0, Text)
+
+    def test_bytes(self):
+        assert isinstance(b"Hello World", Bytes)
+        assert not isinstance(0, Bytes)
