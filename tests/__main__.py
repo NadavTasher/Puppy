880df1e9b0b2471a1270801f90585278dd8518ee
Nadav Tasher <tashernadav@gmail.com>
2023-04-24 16:32:22 +0000

    Deleted socket multiplexing

--- tests/__main__.py
+++ tests/__main__.py
@@ -4,7 +4,6 @@ from test_bunch import *
 from test_database import *
 from test_http import *
 from test_process import *
-from test_socket import *
 from test_thread import *
 from test_event import *
 from test_token import *
